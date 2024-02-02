58ee809fb1b419f654760f5100cda34ba308b152
Gustavo <gustavo_hamister5@hotmail.com>
2024-02-01 23:05:15 -0300

    Impedi envio vazio. Alterei Descriptografar

--- script.js
+++ script.js
@@ -3,37 +3,54 @@ function validarTexto() {
     let valorCampo = campoTexto.value
     // Expressão regular para verificar se há letras maiúsculas, acentos ou caracteres especiais 
     // let validador = /[A-ZÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕÄËÏÖÜÇ!@#$%^&*()_+={}\[\]:;<>,.?~´`0-9\\/]/;
-    let validador = /[^a-z]/;
+    let validador = /[^a-z\s]/;
 
     if (validador.test(valorCampo)) {
         alert(`erro: O texto não deve conter letras Maiúsculas, Acentos ou Caracteres Especiais!`);
         campoTexto.value = '';
         console.log(campoTexto);
+    }   
+}
+
+function validaVazio(texto) {
+    if (texto == '' || texto == ' ') {
+        return true;
     }
+    return false;
 }
 
 function criptografar() {
     let textoEntrada = document.querySelector('#area-text-input');
     let textoSaida = document.querySelector('#area-text-output');
-    let user_text = textoEntrada.value;
+    let user_text = textoEntrada.value.replaceAll(/\s+/g, ' ').trim();
     console.log(user_text);
+
+    if (validaVazio(user_text)) {
+        alert('Precisa de alguma palavra para criptografar');
+        return true   
+    }
 
     let criptografado = user_text.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
     console.log(criptografado);
-
-    textoSaida.value = criptografado;
+   
+    textoSaida.value = criptografado;   
 }
 
 function descriptografar() {
     let textoEntrada = document.querySelector('#area-text-input');
     let textoSaida = document.querySelector('#area-text-output');
-    let user_text = textoEntrada.value;
+    let user_text = textoSaida.value;
     console.log(user_text);
 
+    if (validaVazio(user_text)) {
+        alert('Precisa de alguma palavra para descriptografar');
+        return true   
+    }
+    
     let descriptografado = user_text.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
     console.log(descriptografado);
 
-    textoSaida.value = descriptografado;
+    textoEntrada.value = descriptografado;
 }
 
 async function copiarTexto() {
@@ -42,9 +59,10 @@ async function copiarTexto() {
     
     await navigator.clipboard.writeText(textoCampoSaida);
     campoSaida.value = '';
-
-   
-
 }
 
+/*
+const validaVazio = () => {
 
+}
+ */
